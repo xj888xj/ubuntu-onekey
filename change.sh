02c7754076e7e6aab88ef8df6913e1270772fbe1
renzisen <xj888xj@qq.com>
2021-08-08 13:23:59 +0800

    Add files via upload

---
+++ change.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+cat<<EOF
+                                  _oo0oo_
+                                 088888880
+                                 88" . "88
+                                 (| -_- |)
+                                  0\ = /0
+                               ___/'---'\___
+                             .' \\\\|     |// '.
+                            / \\\\|||  :  |||// \\
+                           /_ ||||| -:- |||||- \\
+                          |   | \\\\\\  -  /// |   |
+                          | \_|  ''\---/''  |_/ |
+                          \  .-\__  '-'  __/-.  /
+                        ___'. .'  /--.--\  '. .'___
+                     ."" '<  '.___\_<|>_/___.' >'  "".
+                    | | : '-  \'.;'\ _ /';.'/ - ' : | |
+                    \  \ '_.   \_ __\ /__ _/   .-' /  /
+                ====='-.____'.___ \_____/___.-'____.-'=====
+                                  '=---='
+
+
+              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+EOF
+
+
+#修改源需要root权限
+if [ $(id -u) -ne 0 ]; then
+    echo "Need Root!"
+    exit 0
+fi
+#备份文件
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
+#替换服务器
+sed -i 's/http:\/\/archive.ubuntu.com/http:\/\/mirrors.aliyun.com/g' /etc/apt/sources.list
+sed -i 's/http:\/\/security.ubuntu.com/http:\/\/mirrors.aliyun.com/g' /etc/apt/sources.list
+#更新源
+apt-get update
+apt-get upgrade
